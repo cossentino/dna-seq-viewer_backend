776b533cca5f63652b05b631bb2bf9637eba424c
Ian Cossentino <icossentino@gmail.com>
2021-08-05 20:53:35 -0400

    improve save view compatibility with front end form; add parse fasta method

--- dna_sequence/models.py
+++ dna_sequence/models.py
@@ -1,10 +1,29 @@
+import pdb
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 
+def validate_sequence(seq_string):
+    for char in seq_string:
+        if char not in ['A', 'T', 'C', 'G']:
+            raise ValidationError(
+                "We came across a character %s that is not valid" % char)
+
+
+def parse_fasta(fasta_string):
+    header = ""
+    i = 0
+    while not fasta_string[i] == "\n":
+        header += fasta_string[i]
+        i += 1
+    sequence = fasta_string[i + 1:]
+    return (header, sequence)
+
+
 class DNASequence(models.Model):
-    raw_sequence = models.TextField(default="")
+    raw_sequence = models.TextField(default="", validators=[validate_sequence])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
--- dna_sequence/views.py
+++ dna_sequence/views.py
@@ -5,8 +5,9 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from django.core.serializers import serialize
+from django.core.exceptions import ValidationError
 from .models import DNASequence
 
 # Create your views here.
@@ -25,8 +26,11 @@ def index(request):
 
 @api_view(('POST',))
 def save(request):
-    attrs = json.loads(request.body)
-    if attrs['sequence_type'] == '1':
-        del attrs['sequence_type']
-        sequence = DNASequence(**attrs)
-    pdb.set_trace()
+    form_data = json.loads(request.body)
+    seq = DNASequence(**form_data)
+    try:
+        seq.full_clean()
+        seq.save()
+    except ValidationError as e:
+        print(e)
+    return HttpResponseRedirect('http://localhost:3000/sequences')
