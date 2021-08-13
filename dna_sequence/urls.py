de9454cfa0622bb7ce37f597119e83e610ff0bff
Ian Cossentino <icossentino@gmail.com>
2021-08-13 14:03:27 -0400

    allow api fetch single sequence

--- dna_sequence/urls.py
+++ dna_sequence/urls.py
@@ -5,5 +5,6 @@ from .views import SequencesView, SequenceView
 app_name = 'dna_sequence'
 
 urlpatterns = [
-    path('sequences', SequencesView.as_view(), name="sequences")
+    path('sequences', SequencesView.as_view(), name="sequences"),
+    path('sequences/<int:sequence_id>', SequenceView.as_view(), name="sequence"),
 ]
--- dna_sequence/views.py
+++ dna_sequence/views.py
@@ -19,15 +19,14 @@ class SequencesView(APIView):
     @csrf_exempt
     def get(self, request):
         user = current_user(request)
-        sequences = list(DNASequence.sequences.filter(pk=user.pk).values())
+        sequences = list(DNASequence.sequences.filter(user=user).values())
         return JsonResponse({'data': sequences})
 
-    @csrf_exempt
     def post(self, request):
-        pdb.set_trace()
+        user = current_user(request)
         form_data = json.loads(request.body)
         del form_data['sequence_type']
-        form_data['user'] = 1
+        form_data['user'] = user
         fasta_header, fasta_seq = parse_fasta(form_data['raw_sequence'])
         form_data['fasta_header'], form_data['raw_sequence'] = fasta_header, fasta_seq
         seq = DNASequence.sequences.create(**form_data)
@@ -36,3 +35,10 @@ class SequencesView(APIView):
 
 class SequenceView(APIView):
     permission_classes = (IsAuthenticated,)
+
+    @csrf_exempt
+    def get(self, request, sequence_id):
+        user = current_user(request)
+        sequence = list(DNASequence.sequences.filter(
+            pk=sequence_id).values())[0]
+        return JsonResponse({'data': sequence})
